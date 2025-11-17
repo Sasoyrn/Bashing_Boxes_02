913ef5e6f1c89367297a5ee2ef647f11750f0907
saso <sogie4greatness@gmail.com>
2025-11-17 14:40:49 -0600

    cleaned up the code

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,71 +1,173 @@
-#!//bin/bash
+#!/bin/bash 
 
-
-
-items=(
-"Keyboard"
-"Television"
-"Sled"
-"Bagel"
-"Eraser"
-"Cotton candy "
-"China cabinet"
-"Casserole dish"
-"Trident"
-"bag"
+random_array=( 
+  "Keyboard"
+  "Television"
+  "Sled"
+  "Bagel"
+  "Eraser"
+  "Cotton candy "
+  "China cabinet"
+  "Casserole dish"
+  "Trident"
+  "bag"
 )
-print_list(){
-  echo "current list (${#items[@]}items):"
-    for i in "${!items[@]}"; do
-    echo "$((i+1)). ${items[$i]}"
+
+
+Print_array() {
+    for item in "${random_array[@]}"; do
+        echo "$item"
     done
-}
-print_x_item(){
-  read -p "which item do you want to print (Your answer must be between 0 and 9: " answer
-  echo "Your item is: ${items[$answer]}"
+    sleep 1
 }
 
-add_item(){
-  read -p "Enter the item you want to add: " new_item
-  items+=("$new_item")
-  echo "You added: $new_item"
-}
-
-remove_last(){
-  echo "Removing the last item: ${items[-1]}"
-  unset 'items[-1]'
-  sleep 1
-  echo "done"
-  sleep 1
-
-}
-
-run=1
-
-
-echo "welcome to OSASOGIE'S array customizer!"
-while [[ $run -eq 1 ]]; do
-    echo "what would you like to do?"
-    echo "1. print the whole list"
-    echo "2. pick an item from the list."
-    echo "3. add item to the list"
-    echo "4. Remove last item from list"
-    echo "5. exit"
-    read -p "what is your choice?" choice
-    if [ $choice -eq 1 ]; then
-      print_list
-    elif [ $choice -eq 2 ]; then
-      print_x_item
-    elif [ $choice -eq 3 ]; then
-      add_item
-    elif [ $choice -eq 4 ]; then
-      remove_last
-    elif [ $choice -eq 5 ]; then
-      echo "exiting..."
-      sleep 2
-
-      exit
+print_single_word() {
+    echo "enter the index of the object you want to print."
+    read -p "tip: The first element on the list is counted as 0." index
+    #this line means if the user put a number that is 0 or higher, and smaller than the number of items in the array, then do the following 
+    if [ "$index" -ge 0 ] && [ "$index" -lt "${#random_array[@]}" ]; then
+        echo "You chose: ${random_array[$index]}"
     else
-      echo "invalid"
+        echo "invalid"
     fi
-done
+    sleep 1
+}
+
+add_item() {
+    read -p "what word do you want to add" word
+    read -p "where do you want to add the item in the array?" pos
+    #checks if index number user gave is in the array
+    if [ "$pos" -ge 0 ] && [ "$pos" -le "${#random_array[@]}" ]; then
+        # Splits the array to add the word 
+        random_array=("${random_array[@]:0:$pos}" "$word" "${random_array[@]:$pos}")
+        echo "Array after insertion: ${random_array[@]}"
+    else
+        echo "Invalid position."
+    fi
+}
+
+delete_item() {
+    echo "enter the index of the word you want to delete"
+    read -p "tip: The first word of array starts with an index of 0 " delete
+    if [ "$delete" -ge 0 ] && [ "$delete" -lt "${#random_array[@]}" ]; then
+        unset 'random_array[delete]'
+        random_array=("${random_array[@]}")
+        echo "Array after deletion : ${random_array[@]}"
+    else
+        echo "invalid index"
+    fi
+}
+
+save_array() {
+    read -p "what would you like to name the file?" new_file
+    printf "%s\n" "${random_array[@]}" > "data/${new_file}.txt"
+    echo "saved as $new_file.txt"
+    sleep 1
+}
+
+load_array() {
+    read -p "Which file would you like to view? " file_to_view
+    if [[ -f "data/${file_to_view}.txt" ]]; then
+        echo "Contents of ${file_to_view}:"
+        cat "data/${file_to_view}.txt"
+        sleep 2
+    else
+        echo "File not found!"
+    fi
+}
+
+List_saved_arrays() {
+    echo "arrays:"
+    if compgen -G "data/*.txt" > /dev/null; then
+        ls data/*.txt | xargs -n 1 basename
+        sleep 1
+    else
+        echo "No saved arrays found."
+        sleep 1
+    fi
+}
+
+Delete_box() {
+    read -p "Which file would you like to delete? " file_to_delete
+    if [[ -f "data/${file_to_delete}.txt" ]]; then
+        rm "data/${file_to_delete}.txt"
+        echo "${file_to_delete}.txt deleted."
+        sleep 1
+    else
+        echo "File not found!"
+        sleep 1
+    fi
+}
+
+
+
+
+echo "Welcome to saso's array customizer!"
+sleep 1 
+
+exiting(){
+   read -p "would you like to save before exiting? (y/n):" exit_read
+   case $exit_read in
+        "y" | "Y") 
+             save_array
+            echo "done!"
+            echo "exiting..."
+            sleep 1
+            exit   
+                ;;
+       "n" | "n")
+                echo "exiting..."
+                sleep 1
+                exit   
+                    ;;
+
+        *)
+            ;;
+    esac 
+}
+
+check_user_input(){
+    case $choice in
+        1) Print_array
+            ;;
+        2) print_single_word
+            ;;
+        3) add_item
+            ;;
+        4) delete_item
+            ;;
+        5) save_array
+            ;;
+        6) load_array
+            ;;
+        7) List_saved_arrays
+            ;;
+        8) Delete_box
+            ;;
+        9) exiting
+            ;;
+        *)
+            ;;
+    esac
+}
+
+
+Display_main_menu(){
+    echo -e "
+    | Main Menu
+    |-----------
+    |1. list all the words
+    |2. print out a word of your liking
+    |3. add an element to the array
+    |4. remove an element from the array
+    |5. save your array
+    |6. load your array
+    |7. list your saved arrays
+    |8. delete your saved array
+    |9. exit"
+    read -p "what is your choice: " choice
+}
+
+Display_main_menu
+check_user_input
+
